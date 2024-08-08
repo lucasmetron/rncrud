bfcd306cbad58d0d825b3ca9d001fb9ac2b5bdfa
Lucas Rosa <lucasmetron@gmail.com>
2024-08-08 13:42:26 -0300

    incio

---
+++ src/views/UserForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const UserForm = () => {
+  return <Text>userform</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default UserForm;
---
+++ src/views/UserList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const UserList = () => {
+  return <Text>Userlist</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default UserList;
